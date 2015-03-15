052d941089f6601f4a65f1d6a7ffc9baf99b10ac
akashvk <akash.kachhiya@gmail.com>
2015-03-15 23:40:52 +0530

    GCM Registeration

--- src/org/app/anoopam/MainActivity.java
+++ src/org/app/anoopam/MainActivity.java
@@ -1,13 +1,17 @@
 package org.app.anoopam;
 
+import com.google.android.gcm.GCMRegistrar;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class MainActivity extends Activity {
-
+	AsyncTask<Void, Void, Void> mRegisterTask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,7 +19,37 @@ public class MainActivity extends Activity {
         this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
         setContentView(R.layout.activity_main);
-
+        
+        
+        	///////////START GCM/////////////////
+		   GCMRegistrar.checkDevice(this);
+		   GCMRegistrar.checkManifest(this);
+			final String regId = GCMRegistrar.getRegistrationId(this);
+		       
+	        if (regId.equals("")) {
+	            GCMRegistrar.register(this, org.app.anoopam.CommonUtilities.SENDER_ID);
+	        } else {
+	        	
+	        	 final Context context = this;
+	             mRegisterTask = new AsyncTask<Void, Void, Void>() {
+	
+	                 @Override
+	                 protected Void doInBackground(Void... params) {
+	                	 GCMRegistrar.setRegisteredOnServer(context, true);
+	                     return null;
+	                 }
+	
+	                 @Override
+	                 protected void onPostExecute(Void result) {
+	                     mRegisterTask = null;
+	                 }
+	
+	             };
+	             mRegisterTask.execute(null, null, null);
+	        }
+	        /////////////END THIS/////////////
+	        
+	        
         Thread background = new Thread() {
             public void run() {
                 try {
